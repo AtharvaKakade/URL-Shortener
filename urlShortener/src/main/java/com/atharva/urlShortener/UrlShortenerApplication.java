8c7f43263815b5eeee2cbc8ad413f14a459dc6ea
AtharvaKakade <kakadeatharva5@gmail.com>
2025-01-16 10:29:14 +0530

    Add CI workflow for Maven project

--- urlShortener/src/main/java/com/atharva/urlShortener/UrlShortenerApplication.java
+++ urlShortener/src/main/java/com/atharva/urlShortener/UrlShortenerApplication.java
@@ -3,10 +3,12 @@ package com.atharva.urlShortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
+//@ComponentScan(basePackages = {"Controller"})
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
